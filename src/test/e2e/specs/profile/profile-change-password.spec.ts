0f232a44c3491cba2a032149ff57be8f552e5409
Alsnj20 <mjarama@unsa.edu.pe>
2026-07-01 17:27:15 -0500

    test: add e2e tests to info profile

---
+++ src/test/e2e/specs/profile/profile-change-password.spec.ts
@@ -0,0 +1,55 @@
+import { expect, test } from "../../fixtures/auth";
+import { withDisposableUser } from "../../helpers/auth-helper";
+import { ProfilePage } from "../../pages/profile/ProfilePage";
+
+test.describe("Owner Profile - Change password", () => {
+    test("user can change their password providing the correct current password", async ({
+        page,
+        adminCredentials,
+        baseURL,
+    }) => {
+        const url = baseURL || "http://localhost:8080";
+
+        await withDisposableUser(
+            page,
+            adminCredentials,
+            url,
+            "OWNER",
+            async (credentials) => {
+                const profile = new ProfilePage(page);
+                await profile.goto();
+
+                await profile.changePassword(
+                    credentials.password,
+                    "Temp1234!x",
+                );
+                expect(await profile.isSuccessMessageVisible()).toBe(true);
+            },
+        );
+    });
+
+    test("shows an error when the current password is incorrect", async ({
+        page,
+        adminCredentials,
+        baseURL,
+    }) => {
+        const url = baseURL || "http://localhost:8080";
+
+        await withDisposableUser(
+            page,
+            adminCredentials,
+            url,
+            "OWNER",
+            async () => {
+                const profile = new ProfilePage(page);
+                await profile.goto();
+
+                await profile.changePassword(
+                    "definitely-wrong-password",
+                    "NewPass123!",
+                );
+                expect(await profile.isOldPasswordErrorVisible()).toBe(true);
+            },
+        );
+    });
+});
